13b1ad3bf46b529336ec10a09d05e79125210490
chemacenturion <centurion.philadelphia@gmail.com>
2021-07-28 21:53:38 -0400

    fixed carousel buttons js

--- assets/script.js
+++ assets/script.js
@@ -68,20 +68,37 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
     targetSlide.classList.add('current-slide');
 }
 
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
     moveToSlide(track, currentSlide, prevSlide);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex)
 });
 
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
     // const amountToMove = nextSlide.style.left;
     //move to the next slide
     // track.style.transform = 'translateX(-' + amountToMove + ')';
     // currentSlide.classList.remove('current-slide');
     // nextSlide.classList.add('current-slide');
     moveToSlide(track, currentSlide, nextSlide);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex)
 });
