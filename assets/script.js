981795d7ce0e4f95df942023dc7922ba6dab1a14
chemacenturion <centurion.philadelphia@gmail.com>
2021-07-28 20:34:04 -0400

    finished repo carousel with js

--- assets/script.js
+++ assets/script.js
@@ -45,6 +45,43 @@ closeWrite.addEventListener('click', () => {
     writeContainer.classList.remove('show');
 });
 
+// insert script.js for carousel here
 
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const slideWidth = slides[0].getBoundingClientRect().width;
 
+//arrange slides next to one and other
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
 
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+});
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    // const amountToMove = nextSlide.style.left;
+    //move to the next slide
+    // track.style.transform = 'translateX(-' + amountToMove + ')';
+    // currentSlide.classList.remove('current-slide');
+    // nextSlide.classList.add('current-slide');
+    moveToSlide(track, currentSlide, nextSlide);
+});
